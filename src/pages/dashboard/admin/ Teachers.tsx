50e25cd30a2a85471be4bda675473e5b5a071a43
elisio42 <elisiouiux@gmail.com>
2025-03-25 10:11:31 +0100

    make modal

--- src/pages/dashboard/admin/ Teachers.tsx
+++ src/pages/dashboard/admin/ Teachers.tsx
@@ -1,15 +1,19 @@
 
 import ComponentCard from "../../../components/common/ComponentCard";
 import PageBreadcrumb from "../../../components/common/PageBreadCrumb";
+import TeacherModalForm from "../../../components/Modals/TeacherModalForm";
 import BasicTableOne from "../../../components/tables/BasicTables/BasicTableOne";
+import { useModal } from "../../../hooks/useModal";
 
 export default function Teachers() {
+  const { openModal } = useModal()  
   return (
     <>
       <PageBreadcrumb pageTitle="Teachers" />
       <div className="space-y-6">
-        <ComponentCard title="Teachers"  click={()=> alert("click me!")} btn={"New teacher"}>
+        <ComponentCard title="Teachers"  click={openModal} btn={"New teacher"}>
           <BasicTableOne />
+          <TeacherModalForm />
         </ComponentCard>
       </div>
     </>
