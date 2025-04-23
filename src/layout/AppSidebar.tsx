4c4d68fd180eca3f32b2395db299e7d33f330037
elisioaugusto <elisiouiux@gmail.com>
2025-04-23 14:29:37 +0100

    notifications

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -55,6 +55,7 @@ const AdminSidebar: React.FC = () => {
       className={`fixed mt-16 md:pt-24 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white 
         dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all 
         duration-300 ease-in-out z-50 border-r border-gray-200
+
         ${isExpanded || isMobileOpen ? "w-[240px]" : "w-[90px]"}
         ${isMobileOpen ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0`}
     >
